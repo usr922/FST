6160e1862cd738ef675cb45935cddcefd0fe6894
Ye Du <83934424+usr922@users.noreply.github.com>
2022-09-16 14:28:22 +0800

    init upload

---
+++ mmseg/models/necks/__init__.py
@@ -0,0 +1,3 @@
+from .segformer_adapter import SegFormerAdapter
+from .featurepyramid import Feature2Pyramid
+__all__ = ['SegFormerAdapter', 'Feature2Pyramid']
---
+++ mmseg/models/uda/source_only.py
@@ -0,0 +1,244 @@
+# The ema model and the domain-mixing are based on:
+# https://github.com/vikolss/DACS
+
+import math
+import os
+import random
+from copy import deepcopy
+
+import mmcv
+import numpy as np
+import torch
+from matplotlib import pyplot as plt
+from timm.models.layers import DropPath
+from torch.nn.modules.dropout import _DropoutNd
+
+from mmseg.core import add_prefix
+from mmseg.models import UDA, build_segmentor
+from mmseg.models.uda.uda_decorator import UDADecorator, get_module
+from mmseg.models.utils.dacs_transforms import (denorm, get_class_masks,
+                                                get_mean_std, strong_transform)
+from mmseg.models.utils.visualization import subplotimg
+from mmseg.utils.utils import downscale_label_ratio
+
+
+def _params_equal(ema_model, model):
+    for ema_param, param in zip(ema_model.named_parameters(),
+                                model.named_parameters()):
+        if not torch.equal(ema_param[1].data, param[1].data):
+            # print("Difference in", ema_param[0])
+            return False
+    return True
+
+
+def calc_grad_magnitude(grads, norm_type=2.0):
+    norm_type = float(norm_type)
+    if norm_type == math.inf:
+        norm = max(p.abs().max() for p in grads)
+    else:
+        norm = torch.norm(
+            torch.stack([torch.norm(p, norm_type) for p in grads]), norm_type)
+
+    return norm
+
+
+@UDA.register_module()
+class SourceOnly(UDADecorator):
+
+    def __init__(self, **cfg):
+        super(SourceOnly, self).__init__(**cfg)
+        self.local_iter = 0
+        self.max_iters = cfg['max_iters']
+        self.alpha = cfg['alpha']
+        self.pseudo_threshold = cfg['pseudo_threshold']
+        self.psweight_ignore_top = cfg['pseudo_weight_ignore_top']
+        self.psweight_ignore_bottom = cfg['pseudo_weight_ignore_bottom']
+        self.fdist_lambda = cfg['imnet_feature_dist_lambda']
+        self.fdist_classes = cfg['imnet_feature_dist_classes']
+        self.fdist_scale_min_ratio = cfg['imnet_feature_dist_scale_min_ratio']
+        self.enable_fdist = self.fdist_lambda > 0
+        self.mix = cfg['mix']
+        self.blur = cfg['blur']
+        self.color_jitter_s = cfg['color_jitter_strength']
+        self.color_jitter_p = cfg['color_jitter_probability']
+        self.debug_img_interval = cfg['debug_img_interval']
+        self.print_grad_magnitude = cfg['print_grad_magnitude']
+        assert self.mix == 'class'
+
+        self.debug_fdist_mask = None
+        self.debug_gt_rescale = None
+
+        self.class_probs = {}
+        ema_cfg = deepcopy(cfg['model'])
+        self.ema_model = build_segmentor(ema_cfg)
+
+        if self.enable_fdist:
+            self.imnet_model = build_segmentor(deepcopy(cfg['model']))
+        else:
+            self.imnet_model = None
+
+    def get_ema_model(self):
+        return get_module(self.ema_model)
+
+    def get_imnet_model(self):
+        return get_module(self.imnet_model)
+
+    def _init_ema_weights(self):
+        for param in self.get_ema_model().parameters():
+            param.detach_()
+        mp = list(self.get_model().parameters())
+        mcp = list(self.get_ema_model().parameters())
+        for i in range(0, len(mp)):
+            if not mcp[i].data.shape:  # scalar tensor
+                mcp[i].data = mp[i].data.clone()
+            else:
+                mcp[i].data[:] = mp[i].data[:].clone()
+
+    def _update_ema(self, iter):
+        alpha_teacher = min(1 - 1 / (iter + 1), self.alpha)
+        for ema_param, param in zip(self.get_ema_model().parameters(),
+                                    self.get_model().parameters()):
+            if not param.data.shape:  # scalar tensor
+                ema_param.data = \
+                    alpha_teacher * ema_param.data + \
+                    (1 - alpha_teacher) * param.data
+            else:
+                ema_param.data[:] = \
+                    alpha_teacher * ema_param[:].data[:] + \
+                    (1 - alpha_teacher) * param[:].data[:]
+
+    def train_step(self, data_batch, optimizer, **kwargs):
+        """The iteration step during training.
+
+        This method defines an iteration step during training, except for the
+        back propagation and optimizer updating, which are done in an optimizer
+        hook. Note that in some complicated cases or models, the whole process
+        including back propagation and optimizer updating is also defined in
+        this method, such as GAN.
+
+        Args:
+            data (dict): The output of dataloader.
+            optimizer (:obj:`torch.optim.Optimizer` | dict): The optimizer of
+                runner is passed to ``train_step()``. This argument is unused
+                and reserved.
+
+        Returns:
+            dict: It should contain at least 3 keys: ``loss``, ``log_vars``,
+                ``num_samples``.
+                ``loss`` is a tensor for back propagation, which can be a
+                weighted sum of multiple losses.
+                ``log_vars`` contains all the variables to be sent to the
+                logger.
+                ``num_samples`` indicates the batch size (when the model is
+                DDP, it means the batch size on each GPU), which is used for
+                averaging the logs.
+        """
+
+        optimizer.zero_grad()
+        log_vars = self(**data_batch)
+        optimizer.step()
+
+        log_vars.pop('loss', None)  # remove the unnecessary 'loss'
+        outputs = dict(
+            log_vars=log_vars, num_samples=len(data_batch['img_metas']))
+        return outputs
+
+    def masked_feat_dist(self, f1, f2, mask=None):
+        feat_diff = f1 - f2
+        # mmcv.print_log(f'fdiff: {feat_diff.shape}', 'mmseg')
+        pw_feat_dist = torch.norm(feat_diff, dim=1, p=2)
+        # mmcv.print_log(f'pw_fdist: {pw_feat_dist.shape}', 'mmseg')
+        if mask is not None:
+            # mmcv.print_log(f'fd mask: {mask.shape}', 'mmseg')
+            pw_feat_dist = pw_feat_dist[mask.squeeze(1)]
+            # mmcv.print_log(f'fd masked: {pw_feat_dist.shape}', 'mmseg')
+        return torch.mean(pw_feat_dist)
+
+    def calc_feat_dist(self, img, gt, feat=None):
+        assert self.enable_fdist
+        with torch.no_grad():
+            self.get_imnet_model().eval()
+            feat_imnet = self.get_imnet_model().extract_feat(img)
+            feat_imnet = [f.detach() for f in feat_imnet]
+        lay = -1
+        if self.fdist_classes is not None:
+            fdclasses = torch.tensor(self.fdist_classes, device=gt.device)
+            scale_factor = gt.shape[-1] // feat[lay].shape[-1]
+            gt_rescaled = downscale_label_ratio(gt, scale_factor,
+                                                self.fdist_scale_min_ratio,
+                                                self.num_classes,
+                                                255).long().detach()
+            fdist_mask = torch.any(gt_rescaled[..., None] == fdclasses, -1)
+            feat_dist = self.masked_feat_dist(feat[lay], feat_imnet[lay],
+                                              fdist_mask)
+            self.debug_fdist_mask = fdist_mask
+            self.debug_gt_rescale = gt_rescaled
+        else:
+            feat_dist = self.masked_feat_dist(feat[lay], feat_imnet[lay])
+        feat_dist = self.fdist_lambda * feat_dist
+        feat_loss, feat_log = self._parse_losses(
+            {'loss_imnet_feat_dist': feat_dist})
+        feat_log.pop('loss', None)
+        return feat_loss, feat_log
+
+    def forward_train(self, img, img_metas, gt_semantic_seg, target_img,
+                      target_img_metas):
+        """Forward function for training.
+
+        Args:
+            img (Tensor): Input images.
+            img_metas (list[dict]): List of image info dict where each dict
+                has: 'img_shape', 'scale_factor', 'flip', and may also contain
+                'filename', 'ori_shape', 'pad_shape', and 'img_norm_cfg'.
+                For details on the values of these keys see
+                `mmseg/datasets/pipelines/formatting.py:Collect`.
+            gt_semantic_seg (Tensor): Semantic segmentation masks
+                used if the architecture supports semantic segmentation task.
+
+        Returns:
+            dict[str, Tensor]: a dictionary of loss components
+        """
+        log_vars = {}
+        batch_size = img.shape[0]
+        dev = img.device
+
+        # Init/update ema model
+        if self.local_iter == 0:
+            self._init_ema_weights()
+            # assert _params_equal(self.get_ema_model(), self.get_model())
+
+        if self.local_iter > 0:
+            self._update_ema(self.local_iter)
+            # assert not _params_equal(self.get_ema_model(), self.get_model())
+            # assert self.get_ema_model().training
+
+        means, stds = get_mean_std(img_metas, dev)
+        strong_parameters = {
+            'mix': None,
+            'color_jitter': random.uniform(0, 1),
+            'color_jitter_s': self.color_jitter_s,
+            'color_jitter_p': self.color_jitter_p,
+            'blur': random.uniform(0, 1) if self.blur else 0,
+            'mean': means[0].unsqueeze(0),  # assume same normalization
+            'std': stds[0].unsqueeze(0)
+        }
+
+        # Train on source images
+        clean_losses = self.get_model().forward_train(
+            img, img_metas, gt_semantic_seg, return_feat=True)
+        src_feat = clean_losses.pop('features')
+        clean_loss, clean_log_vars = self._parse_losses(clean_losses)
+        log_vars.update(clean_log_vars)
+        clean_loss.backward(retain_graph=self.enable_fdist)
+        if self.print_grad_magnitude:
+            params = self.get_model().backbone.parameters()
+            seg_grads = [
+                p.grad.detach().clone() for p in params if p.grad is not None
+            ]
+            grad_mag = calc_grad_magnitude(seg_grads)
+            mmcv.print_log(f'Seg. Grad.: {grad_mag}', 'mmseg')
+
+ 
+        self.local_iter += 1
+
+        return log_vars
